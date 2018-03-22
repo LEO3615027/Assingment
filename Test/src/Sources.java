60be00e386d545ebd0e6967beeeea6e9664c2968
s3615027 <s3615027@XAPPRDAPBN207.rmit.internal>
2018-03-22 21:14:08 +1100

    ty

---
+++ Test/src/Sources.java
@@ -0,0 +1,11 @@
+https://unilearning.uow.edu.au/report/4b.html
+
+http://libweb.surrey.ac.uk/library/skills/Introduction%20to%20Research%20and%20Managing%20Information%20Leicester/page_79.htm
+
+https://www.nfer.ac.uk/schools/research-in-schools/presenting-your-findings/
+
+https://student.unsw.edu.au/report-writing-support
+
+XX https://www.openpolytechnic.ac.nz/current-students/study-tips-and-techniques/assignments/how-to-write-a-report/#information
+
+http://www.bbc.co.uk/skillswise/topic-group/graphs
